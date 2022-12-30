d8741bcbdaf704d85082eed602c1e066707f5639
Monday Solomon <83866674+black-coda@users.noreply.github.com>
2022-12-30 22:10:25 +0100

    RedirectView added

--- favorite/urls.py
+++ favorite/urls.py
@@ -1,16 +1,22 @@
 from django.contrib import admin
 from django.urls import path, include
-
 from django.shortcuts import render
+from django.views.generic import RedirectView
+
 
 def home(request):
     words = "solomon"
     return render(request, "index.html")
 
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
+    path("api-auth/", include("rest_framework.urls")),
     path("api/v1/", include("main.urls")),
-    path('',home)
-
+    path(
+        "",
+        RedirectView.as_view(
+            url="api/v1/",
+        ),
+    ),
 ]
